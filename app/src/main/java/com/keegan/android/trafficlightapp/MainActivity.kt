88cd862c09bb1a5000c376d30fa3d44b01ffa0fd
Keegan <kgm1328@live.missouristate.edu>
2022-09-17 13:07:34 -0500

    Added button and basic button functionality to activity_main.xml and MainActivity.kt

--- app/src/main/java/com/keegan/android/trafficlightapp/MainActivity.kt
+++ app/src/main/java/com/keegan/android/trafficlightapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.keegan.android.trafficlightapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var change_Button :AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        change_Button = findViewById(R.id.changeButton)
+
+        change_Button.setOnClickListener {
+            var value = change_Button.text.toString()
+
+            if(value.equals("STOP")){
+                //switch to green
+                change_Button.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+                change_Button.setText(R.string.go)
+            }
+            else if(value.equals("GO")){
+                //switch to yellow
+                change_Button.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow))
+                change_Button.setText(R.string.wait)
+            }
+            else{ //switch to red
+                change_Button.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+                change_Button.setText(R.string.stop)
+            }
+        }
     }
 }
